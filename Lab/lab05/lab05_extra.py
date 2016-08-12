b78dbf9835b0005b3822e7517ac6ed0498dbdaa9
JasonVann <jasonvanet@gmail.com>
2016-08-11 22:27:52 -0700

    Added 6 new sols to Lab05 extra

--- Lab/lab05/lab05_extra.py
+++ Lab/lab05/lab05_extra.py
@@ -15,7 +15,12 @@ def sum_linked_list(lst, fn):
     >>> sum_linked_list(lst2, double)
     44
     """
-    "*** YOUR CODE HERE ***"
+    res = 0
+    
+    while lst != empty and first(lst) != empty:
+        res += fn(first(lst))
+        lst = rest(lst)
+    return res
 
 def change(lst, s, t):
     """Returns a link matching lst but with all instances of s 
@@ -32,7 +37,16 @@ def change(lst, s, t):
     >>> print_link(newest)
     2 2 2
     """
-    "*** YOUR CODE HERE ***"
+    if lst == empty:
+        return lst
+    else:
+        #print('a', first(lst), rest(lst))
+        if first(lst) == s:
+            #print('b', s, rest(lst))
+            return link(t, change(rest(lst), s, t))
+        else:
+            return link(first(lst), change(rest(lst), s, t))
+
 
 def link_to_list(linked_lst):
     """Return a list that contains the values inside of linked_lst
@@ -43,7 +57,10 @@ def link_to_list(linked_lst):
     >>> link_to_list(lst1)
     [1, 2, 3]
     """
-    "*** YOUR CODE HERE ***"
+    if linked_lst == empty:
+        return []
+    else:
+        return [first(linked_lst)] + link_to_list(rest(linked_lst))
 
 def insert(lst, item, index):
     """Returns a link matching lst but with the given item inserted at the
@@ -58,7 +75,13 @@ def insert(lst, item, index):
     >>> print_link(newer)
     1 9001 2 3 9002
     """
-    "*** YOUR CODE HERE ***"
+    #print('a', index, lst)
+    if index >= 0 and lst == empty:
+        return link(item, empty)
+    elif index == 0:
+        return link(item, lst)
+    else:
+        return link(first(lst), insert(rest(lst), item, index - 1))
 
 def interleave(s0, s1):
     """Interleave linked lists s0 and s1 to produce a new linked
@@ -73,7 +96,32 @@ def interleave(s0, s1):
     >>> print_link(interleave(odds, odds))
     1 1 3 3
     """
-    "*** YOUR CODE HERE ***"
+    if s0 == empty:
+        return s1
+    elif s1 == empty:
+        return s0
+
+    return link(first(s0), link(first(s1), interleave(rest(s0), rest(s1))))
+
+# Iterative version
+# Sol from webpage
+def interleave_iter(s0, s1):
+    interleaved = empty
+    while s0 != empty and s1 != empty:
+        interleaved = link(first(s1), link(first(s0), interleaved))
+        s0, s1 = rest(s0), rest(s1)
+    remaining = s1 if s0 == empty else s0
+    while remaining != empty:
+        interleaved = link(first(remaining), interleaved)
+        remaining = rest(remaining)
+    return reverse_iterative0(interleaved)
+
+def reverse_iterative0(s):
+    rev_list = empty
+    while s != empty:
+        rev_list = link(first(s), rev_list)
+        s = rest(s)
+    return rev_list
 
 def filter_list(predicate, lst):
     """Returns a link only containing elements in lst that satisfy
@@ -84,7 +132,13 @@ def filter_list(predicate, lst):
     >>> print_link(new)
     50 80
     """
-    "*** YOUR CODE HERE ***"
+    if lst == empty:
+        return lst
+    else:
+        if predicate(first(lst)):
+            return link(first(lst), filter_list(predicate, rest(lst)))
+        else:
+            return filter_list(predicate, rest(lst))
 
 def reverse_iterative(s):
     """Return a reversed version of a linked list s.
