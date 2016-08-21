a53b171a72031886df8c43246f1a57244f34e0fd
JasonVann <jasonvanet@gmail.com>
2016-08-20 18:02:03 -0700

    Added phase0 for maps

--- Project/maps/utils.py
+++ Project/maps/utils.py
@@ -16,8 +16,8 @@ def map_and_filter(s, map_fn, filter_fn):
     [1, 9, 25]
     """
     # BEGIN Question 0
-    "*** REPLACE THIS LINE ***"
-    return ['REPLACE THIS WITH YOUR LIST COMPREHENSION']
+    return [map_fn(x) for x in s if filter_fn(x)]
+
     # END Question 0
 
 def key_of_min_value(d):
@@ -30,8 +30,8 @@ def key_of_min_value(d):
     'c'
     """
     # BEGIN Question 0
-    "*** REPLACE THIS LINE ***"
-    return min('REPLACE THIS WITH YOUR SOLUTION')
+    return [a for a, b in d.items() if b == min(d.values())][0]
+
     # END Question 0
 
 def zip(*sequences):
@@ -63,7 +63,7 @@ def enumerate(s, start=0):
     [[5, 'f'], [6, 'i'], [7, 'v'], [8, 'e']]
     """
     # BEGIN Question 0
-    "*** REPLACE THIS LINE ***"
+    return [[i, s[i - start]] for i in range(start, start + len(s))]
     # END Question 0
 
 def distance(pos1, pos2):
